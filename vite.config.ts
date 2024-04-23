6ae15138e11d6e7be23c5c53d5da3e93f963290a
SnailRun <3074994545@qq.com>
2024-04-23 09:26:45 +0800

    修复终端无法运行bug

--- vite.config.ts
+++ vite.config.ts
@@ -11,30 +11,33 @@ export default defineConfig({
       name: "FileSizeCheck",
       fileName: (format) => `file-size-check.${format}.js`,
     },
+    outDir: "./dist",
     rollupOptions: {
       plugins: [
-        terser() as Plugin, // 用于代码压缩
-        obfuscator({
-          // 用于代码混淆
-          compact: true,
-          controlFlowFlattening: true,
-          controlFlowFlatteningThreshold: 0.75,
-          deadCodeInjection: true,
-          deadCodeInjectionThreshold: 0.4,
-          debugProtection: true,
-          debugProtectionInterval: false,
-          disableConsoleOutput: true,
-          rotateStringArray: true,
-          stringArray: true,
-          stringArrayEncoding: ["rc4"],
-          stringArrayThreshold: 0.75,
-        }) as Plugin,
+        // terser() as Plugin, // 用于代码压缩
+        // obfuscator({
+        //   // 用于代码混淆
+        //   compact: true,
+        //   controlFlowFlattening: true,
+        //   controlFlowFlatteningThreshold: 0.75,
+        //   deadCodeInjection: true,
+        //   deadCodeInjectionThreshold: 0.4,
+        //   debugProtection: true,
+        //   debugProtectionInterval: false,
+        //   disableConsoleOutput: true,
+        //   rotateStringArray: true,
+        //   stringArray: true,
+        //   stringArrayEncoding: ["rc4"],
+        //   stringArrayThreshold: 0.75,
+        // }) as Plugin,
       ],
-      external: ["fs", "path"],
+      external: ["fs", "path", "glob", "yargs"],
       output: {
         globals: {
           fs: "fs",
           path: "path",
+          glob: "glob",
+          yargs: "yargs",
         },
       },
     },
